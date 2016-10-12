494540471f065f668456b4668e4564c3eb9651a5
Tyler <tamano@chadwickschool.org>
2016-10-11 21:35:47 -0700

    Add dance steps for intro

--- partone.py
+++ partone.py
@@ -1,2 +1,18 @@
 from Myro import *
 init("sim")
+def CircleyCircle(x):
+    penDown()
+    move(.5,1)
+    wait(x)
+    stop()
+    penUp()
+def swayIntro(y):
+    while y > 0:
+        turnLeft(6,.5)
+        turnRight(6,.5)
+        y=y-1
+CircleyCircle(6)
+forward(1,1.1)
+swayIntro(6)
+turnRight(4,1)
+
