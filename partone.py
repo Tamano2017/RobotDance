ecad009df24bd296753a901d9091dfa9150f1827
Tyler <tamano@chadwickschool.org>
2016-10-13 11:48:31 -0700

    Change while loops to for loops and merge a few dance steps into one function

--- partone.py
+++ partone.py
@@ -7,34 +7,40 @@ def CircleyCircle(x):
     stop()
     penUp()
 def swayRhythm(x):
-    while x > 0:
+    for i in range(0,x):
         turnLeft(5,.42)
         stop()
         turnRight(5,.42)
         stop()
-        x=x-1
 def sharpTurn(x):
-    while x > 0:
+    for i in range(0,x):
         turnRight(9,.2)
-        x=x-1
-        wait(.35)
+        wait(.4)
 def slide(x):
     forward(2,x)
     stop()
 def back(x):
     backward(2,x)
     stop()
+def TaeVerseEnd(x):
+    for i in range (0,x):
+        slide(.42)
 CircleyCircle(6)
 forward(1,1.1)
 swayRhythm(6)
 turnRight(4,2.2)
 swayRhythm(6)
 sharpTurn(3)
-wait(.2)
-slide(1.1)
-back(1.1)
-slide(.42)
-back(.42)
-slide(.42)
+wait(.1)
+slide(.8)
+back(.8)
+TaeVerseEnd(2)
+CircleyCircle(4)
+back(2)
+slide(1)
+sharpTurn(8)
+CircleyCircle(5)
+
+
 
 
